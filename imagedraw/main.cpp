fa03fe2eb2ed336ed49ffab0ef155202f16e2ab8
v2ray <2282688304@qq.com>
2025-04-20 08:15:02 +0000

    Generalized byte replace bit to multiple bits at once.

--- imagedraw/main.cpp
+++ imagedraw/main.cpp
@@ -30,42 +30,34 @@ inline void set_border_color(c64color_t color) {
     *bcr = static_cast<uint8_t>(color);
 }
 
-uint8_t rotate_left(uint8_t x, uint8_t shift) {
-    constexpr uint8_t bit_length = sizeof(x) * CHAR_BIT;
-    // shift %= bit_length; // Assume shift <= bit_length.
-    return x << shift | x >> (bit_length - shift);
-}
-
 bool byte_select_bit(uint8_t source, uint8_t idx) {
-    return source & rotate_left(1, idx);
+    return source & 1 << idx;
 }
 
-uint8_t byte_replace_bit(uint8_t source, uint8_t idx, bool target) {
-    if (target)
-        return source | rotate_left(1, idx);
-    else
-        return source & rotate_left(0b11111110, idx);
+uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t target, uint8_t target_len = 1) {
+    uint8_t mask = ~(0b11111111 >> (CHAR_BIT - target_len) << offset);
+    return (source & mask) | target << offset;
 }
 
 // __attribute__((noinline))
 void set_graphics_mode(c64graphics_t graphics_mode) {
     auto scr1 = MAKE_VTPTR(uint8_t, 0xd011);
-    *scr1 = byte_replace_bit(*scr1, 6, byte_select_bit(static_cast<uint8_t>(graphics_mode), 2));
+    *scr1 = byte_replace_bits(*scr1, 6, byte_select_bit(static_cast<uint8_t>(graphics_mode), 2));
 
     bool bmm = byte_select_bit(static_cast<uint8_t>(graphics_mode), 1);
 
     // If using bitmap mode, move bitmap memory to 0x6000 and screen memory (color) to 0x5c00.
     auto vbp = MAKE_VTPTR(uint8_t, 0xdd00);
-    *vbp = byte_replace_bit(*vbp, 0, !bmm);
+    *vbp = byte_replace_bits(*vbp, 0, !bmm);
     auto vmo = MAKE_VTPTR(uint8_t, 0xd018);
-    *vmo = byte_replace_bit(*vmo, 3, bmm);
-    *vmo = byte_replace_bit(*vmo, 5, bmm);
-    *vmo = byte_replace_bit(*vmo, 6, bmm);
+    *vmo = byte_replace_bits(*vmo, 3, bmm);
+    *vmo = byte_replace_bits(*vmo, 5, bmm);
+    *vmo = byte_replace_bits(*vmo, 6, bmm);
 
-    *scr1 = byte_replace_bit(*scr1, 5, bmm);
+    *scr1 = byte_replace_bits(*scr1, 5, bmm);
 
     auto scr2 = MAKE_VTPTR(uint8_t, 0xd016);
-    *scr2 = byte_replace_bit(*scr2, 4, byte_select_bit(static_cast<uint8_t>(graphics_mode), 0));
+    *scr2 = byte_replace_bits(*scr2, 4, byte_select_bit(static_cast<uint8_t>(graphics_mode), 0));
 }
 
 void memset_img_sbm(const uint8_t* data, c64ptr_t bitmap_mem_addr, c64ptr_t screen_mem_addr) {
