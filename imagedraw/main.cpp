3e33b7cbdfa636b228ef428ec378fc01c4cc7602
v2ray <2282688304@qq.com>
2025-04-20 10:15:37 +0000

    Made selecting VIC memory position easier.

--- imagedraw/main.cpp
+++ imagedraw/main.cpp
@@ -34,9 +34,37 @@ bool byte_select_bit(uint8_t source, uint8_t idx) {
     return source & 1 << idx;
 }
 
-uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t target, uint8_t target_len = 1) {
+constexpr uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t target, uint8_t target_len = 1) {
     uint8_t mask = ~(0b11111111 >> (CHAR_BIT - target_len) << offset);
     return (source & mask) | target << offset;
+}
+
+template<c64ptr_t vbp_start_addr>
+constexpr uint8_t get_vic_bank_partition_bits() {
+    static_assert(vbp_start_addr % 0x4000 == 0, "VIC bank partition must start at multiple of 0x4000!");
+    return static_cast<uint8_t>(~byte_replace_bits(vbp_start_addr / 0x4000, 2, 0b00111111, 6));
+}
+
+template<c64ptr_t screen_start_offset, c64ptr_t bitmap_start_offset, c64ptr_t character_start_offset = 0xffff>
+constexpr uint8_t get_vic_memory_offset() {
+    static_assert(screen_start_offset < 0x4000, "Screen memory offset must be less than 0x4000, since this is an offset inside the VIC bank partition!");
+    static_assert(screen_start_offset % 0x0400 == 0, "Screen memory offset must start at multiple of 0x0400!");
+
+    constexpr bool bitmap_provided = bitmap_start_offset != 0xffff;
+    constexpr bool chara_provided = character_start_offset != 0xffff;
+    static_assert(bitmap_provided ^ chara_provided, "You can only set one of bitmap start offset or character start offset, since they override each other!");
+
+    uint8_t result = screen_start_offset / 0x0400 << 4;
+    if constexpr (bitmap_provided) {
+        static_assert(bitmap_start_offset < 0x4000, "Bitmap memory offset must be less than 0x4000, since this is an offset inside the VIC bank partition!");
+        static_assert(bitmap_start_offset % 0x2000 == 0, "Bitmap memory offset must start at multiple of 0x2000!");
+        result = byte_replace_bits(result, 3, bitmap_start_offset / 0x2000);
+    } else if (chara_provided) {
+        static_assert(character_start_offset < 0x4000, "Character memory offset must be less than 0x4000, since this is an offset inside the VIC bank partition!");
+        static_assert(character_start_offset % 0x0800 == 0, "Character memory offset must start at multiple of 0x0800!");
+        result = byte_replace_bits(result, 1, character_start_offset / 0x0800, 3);
+    }
+    return result;
 }
 
 // __attribute__((noinline))
@@ -48,11 +76,9 @@ void set_graphics_mode(c64graphics_t graphics_mode) {
 
     // If using bitmap mode, move bitmap memory to 0x6000 and screen memory (color) to 0x5c00.
     auto vbp = MAKE_VTPTR(uint8_t, 0xdd00);
-    *vbp = byte_replace_bits(*vbp, 0, !bmm);
+    *vbp = byte_replace_bits(*vbp, 0, bmm ? get_vic_bank_partition_bits<0x4000>() : get_vic_bank_partition_bits<0>(), 2);
     auto vmo = MAKE_VTPTR(uint8_t, 0xd018);
-    *vmo = byte_replace_bits(*vmo, 3, bmm);
-    *vmo = byte_replace_bits(*vmo, 5, bmm);
-    *vmo = byte_replace_bits(*vmo, 6, bmm);
+    *vmo = bmm ? get_vic_memory_offset<0x1c00, 0x2000>() : get_vic_memory_offset<0x0400, 0xffff, 0x1800>();
 
     *scr1 = byte_replace_bits(*scr1, 5, bmm);
 
