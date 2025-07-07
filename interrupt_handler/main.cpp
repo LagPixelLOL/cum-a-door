c44ff191a98b644115c103887be937ecd5467054
v2ray <2282688304@qq.com>
2025-07-07 15:13:55 +0000

    Made the interrupt handler demo look better.

--- interrupt_handler/main.cpp
+++ interrupt_handler/main.cpp
@@ -1,28 +1,68 @@
-#include <cstdio>
+#include <cstdint>
+#include <climits>
 
 using c64ptr_t = uint16_t;
 #define MAKE_VTPTR(typename, address) reinterpret_cast<volatile typename*>(static_cast<c64ptr_t>(address))
 
-void asdf() {
-    auto f = MAKE_VTPTR(uint8_t, 0x0400);
-    *f = 1;
-    asm volatile ("");
+enum class c64color_t : uint8_t {
+    BLACK, WHITE, RED, CYAN,
+    PURPLE, GREEN, BLUE, YELLOW,
+    ORANGE, BROWN, PINK, DARK_GREY,
+    GREY, LIGHT_GREEN, LIGHT_BLUE, LIGHT_GREY,
+    END, START = BLACK
+};
+
+inline void set_border_color(c64color_t color) {
+    auto bcr = MAKE_VTPTR(uint8_t, 0xd020);
+    *bcr = static_cast<uint8_t>(color);
 }
 
-void ghjk() {
-    auto f = MAKE_VTPTR(uint8_t, 0x0401);
-    *f = 2;
-    asm volatile ("");
+constexpr uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t target, uint8_t target_len = 1) {
+    uint8_t mask = ~(0b11111111 >> (CHAR_BIT - target_len) << offset);
+    return (source & mask) | target << offset;
+}
+
+inline void wait_for_n_loops(uint16_t n_loops) {
+    for (uint16_t i = 0; i < n_loops; ++i) {
+        asm ("");
+    }
+}
+
+inline void set_raster_interrupt_line(uint8_t line) {
+    auto crl = MAKE_VTPTR(uint8_t, 0xd012);
+    *crl = line;
 }
 
 __attribute__((interrupt)) __attribute__((no_isr)) void interrupt_handler() {
-    asdf();
-    ghjk();
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(14);
+    set_border_color(c64color_t::RED);
+    wait_for_n_loops(12);
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(12);
+    set_border_color(c64color_t::ORANGE);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::YELLOW);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::GREEN);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(12);
+    set_border_color(c64color_t::CYAN);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::DARK_GREY);
+    wait_for_n_loops(11);
+    set_border_color(c64color_t::BLACK);
 }
 
 __attribute__((naked)) void bare_interrupt_caller() {
-    asm volatile (
+    asm (
         "jsr %0\n"
+        "asl 0xd019\n"
         "jmp 0xea31\n"
         :
         : "i"(interrupt_handler)
@@ -30,12 +70,19 @@ __attribute__((naked)) void bare_interrupt_caller() {
 }
 
 int main() {
-    asm volatile ("sei");
+    asm ("sei");
+    uint8_t cia_flag = 0b01111111;
+    auto cia1 = MAKE_VTPTR(uint8_t, 0xdc0d);
+    *cia1 = cia_flag;
+    // auto cia2 = MAKE_VTPTR(uint8_t, 0xdd0d);
+    // *cia2 = cia_flag;
+    auto scr = MAKE_VTPTR(uint8_t, 0xd011);
+    *scr = byte_replace_bits(*scr, 7, 0);
+    set_raster_interrupt_line(14);
     auto isr = MAKE_VTPTR(c64ptr_t, 0x0314);
     *isr = reinterpret_cast<c64ptr_t>(bare_interrupt_caller);
-    asm volatile ("cli");
-    while (true) {
-        printf("x");
-    }
+    auto icr = MAKE_VTPTR(uint8_t, 0xd01a);
+    *icr = 1;
+    asm ("cli");
     return 0;
 }
