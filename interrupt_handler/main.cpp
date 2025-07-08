7d282e4f64ce6e24757be14b808a6135bdfa7d25
v2ray <2282688304@qq.com>
2025-07-08 15:54:08 +0000

    Added precise timing.

--- interrupt_handler/main.cpp
+++ interrupt_handler/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <climits>
+#include <cstdlib>
 
 using c64ptr_t = uint16_t;
 #define MAKE_VTPTR(typename, address) reinterpret_cast<volatile typename*>(static_cast<c64ptr_t>(address))
@@ -13,8 +14,12 @@ enum class c64color_t : uint8_t {
 };
 
 inline void set_border_color(c64color_t color) {
-    auto bcr = MAKE_VTPTR(uint8_t, 0xd020);
-    *bcr = static_cast<uint8_t>(color);
+    asm (
+        "ldx #%0\n"
+        "stx 0xd020"
+        :
+        : "n"(color)
+    );
 }
 
 constexpr uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t target, uint8_t target_len = 1) {
@@ -22,11 +27,65 @@ constexpr uint8_t byte_replace_bits(uint8_t source, uint8_t offset, uint8_t targ
     return (source & mask) | target << offset;
 }
 
-inline void wait_for_n_loops(uint16_t n_loops) {
-    for (uint16_t i = 0; i < n_loops; ++i) {
-        asm ("");
+template<size_t N>
+__attribute__((always_inline)) inline void nops() {
+    #pragma unroll
+    for (size_t i = 0; i < N; ++i) {
+        asm ("nop");
     }
 }
+
+template<size_t N>
+__attribute__((always_inline)) inline void cycles_1() {
+    static_assert(N >= 2, "N must be at least 2!");
+    constexpr bool exact = N % 2 == 0;
+    constexpr size_t n_nop = exact ? N / 2 : N / 2 - 1;
+    nops<n_nop>();
+    if constexpr (exact) {
+        return;
+    }
+    asm (
+        "jmp next%=\n"
+        "next%=:"
+        :
+    );
+}
+
+template<size_t N>
+__attribute__((always_inline)) inline void cycles_2() {
+    static_assert(N >= 2, "N must be at least 2!");
+    constexpr size_t n_cycle = N - 2;
+    constexpr bool exact = n_cycle % 5 == 4;
+    constexpr bool single_remained = n_cycle % 5 == 0;
+    constexpr size_t n_loop = N < 4 ? 0 : single_remained ? n_cycle / 5 - 1 : exact ? n_cycle / 5 + 1 : n_cycle / 5;
+    if constexpr (n_loop) {
+        asm (
+            "ldx #%0\n"
+            "loop%=:\n"
+            "dex\n"
+            "bne loop%="
+            :
+            : "n"(n_loop)
+        );
+    }
+    constexpr size_t remainder = n_loop ? n_cycle - n_loop * 5 + 1 : n_cycle + 2;
+    if constexpr (!remainder) {
+        return;
+    }
+    constexpr bool remainder_exact = remainder % 2 == 0;
+    constexpr size_t n_left = remainder_exact ? remainder / 2 : remainder / 2 - 1;
+    nops<n_left>();
+    if constexpr (remainder_exact) {
+        return;
+    }
+    asm (
+        "jmp next%=\n"
+        "next%=:"
+        :
+    );
+}
+
+#define cycles cycles_2
 
 inline void set_raster_interrupt_line(uint8_t line) {
     auto crl = MAKE_VTPTR(uint8_t, 0xd012);
@@ -34,28 +93,29 @@ inline void set_raster_interrupt_line(uint8_t line) {
 }
 
 __attribute__((interrupt)) __attribute__((no_isr)) void interrupt_handler() {
+    cycles<11>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(14);
+    cycles<57>();
     set_border_color(c64color_t::RED);
-    wait_for_n_loops(12);
+    cycles<57>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(12);
+    cycles<57>();
     set_border_color(c64color_t::ORANGE);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::YELLOW);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::GREEN);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(12);
+    cycles<57>();
     set_border_color(c64color_t::CYAN);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::DARK_GREY);
-    wait_for_n_loops(11);
+    cycles<57>();
     set_border_color(c64color_t::BLACK);
 }
 
@@ -63,7 +123,7 @@ __attribute__((naked)) void bare_interrupt_caller() {
     asm (
         "jsr %0\n"
         "asl 0xd019\n"
-        "jmp 0xea31\n"
+        "jmp 0xea31"
         :
         : "i"(interrupt_handler)
     );
